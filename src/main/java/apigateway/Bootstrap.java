2f153b61cf089f7558234f936c9670b49e1d73c8
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-09 23:12:55 +0530

    Auth token

--- src/main/java/apigateway/Bootstrap.java
+++ src/main/java/apigateway/Bootstrap.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
+@CrossOrigin(origins = "*")
 public class Bootstrap {
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class,args);
