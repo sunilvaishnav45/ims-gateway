96286ff9c89d99ab6f435a317a40e5d6c106fc3f
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-09 20:53:16 +0530

    Auth token

--- src/main/java/apigateway/config/CorsConfiguration.java
+++ src/main/java/apigateway/config/CorsConfiguration.java
@@ -20,7 +20,7 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins(getOrigin())
                         .allowedMethods("GET", "POST","PUT", "DELETE");
             }
