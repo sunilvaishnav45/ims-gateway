dbf69d60fb59c024132e761dcdf93a34b1048ab1
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-09 23:07:56 +0530

    Auth token

--- src/main/java/apigateway/config/CorsConfiguration.java
+++ src/main/java/apigateway/config/CorsConfiguration.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsConfiguration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer
 {
     @Value("#{'${allowed.cross.origin}'.split(',')}")
     private List<String> allowedCrossOrigin;
